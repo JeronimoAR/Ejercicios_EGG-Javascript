2a767b2d87b4fe724f852644e7477d6a14b8f827
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2024-02-13 10:23:28 +0200

    Keyboard Input

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const numberBtns = document.querySelectorAll('#Number')
+const operators = document.querySelectorAll('#operator')
 let botones = document.getElementsByClassName('buttons');
 let operation = document.getElementById('operation');
 let result = document.getElementById('result');
@@ -50,114 +52,11 @@ function divNumbers(num1, num2) {
   }
 }
 
-const operate = (e) => {
-  // operate function - we can rework this and make sure to include floating point numbers and operations with more than 2 numbers
-  // we also need to rework this to avoid using innerHTML if possible, and see if we can simplify this method
-  // we need to rework this function to also accept keyboard number inputs
-  //<------------ original logic from Jero's forked repo ------------>//
-  let op = operation.value.toString();
-  console.log(op);
-  if (e.key === 'Enter') {
-    sum = op.indexOf('+');
-    res = op.indexOf('-');
-    mul = op.indexOf('*');
-    div = op.indexOf('/');
-    if (sum !== -1) {
-      num1 = op.substring(0, sum);
-      num2 = op.substring(sum + 1);
-      res = addNumbers(num1, num2);
-    } else if (res > 0) {
-      num1 = parseInt(op.substring(0, res));
-      num2 = parseInt(op.substring(res + 1, op.length));
-      res = num1 - num2;
-    } else if (mul > 0) {
-      num1 = parseInt(op.substring(0, mul));
-      num2 = parseInt(op.substring(mul + 1, op.length));
-      res = num1 * num2;
-    } else if (div > 0) {
-      num1 = parseInt(op.substring(0, div));
-      num2 = parseInt(op.substring(div + 1, op.length));
-      res = num1 / num2;
-    }
-    op = '0';
-    resultado.innerHTML = res;
-
-    operation.value = op;
-    if (operation.value == '') {
-      operation.ariaPlaceholder = '0';
-    }
-  }
-  //<------------ we need to rework this to apply what branko taught us and to extend functionality to floating point numbers ------------>//
-};
-
 const initialiseClickHandler = () => {
   // this function should add the event listeners for the clickable UI
   operation.addEventListener('click', (e) => operate(e));
 };
 
-const initialiseKeyboardHandler = () => {
-  // we need to also add event listeners for keyboard responsiveness
-  operation.addEventListener('keyup', (e) => operate(e));
-};
-
-
-//<------------ original logic from Jero's forked repo ------------>//
-
-/*for (let i = 0; i < botones.length; i++) {
-  let element = botones[i];
-  element.addEventListener('click', (e) => {
-    let op = element.innerHTML.toString();
-    console.log(op);
-    if (op == '=') {
-      sum = completo.indexOf('+');
-      res = completo.indexOf('-');
-      mul = completo.indexOf('*');
-      div = completo.indexOf('/');
-      if (sum > 0) {
-        num1 = parseInt(completo.substring(0, sum));
-        num2 = parseInt(completo.substring(sum + 1, completo.length));
-        res = num1 + num2;
-      } else if (res > 0) {
-        num1 = parseInt(completo.substring(0, res));
-        num2 = parseInt(completo.substring(res + 1, completo.length));
-        res = num1 - num2;
-      } else if (mul > 0) {
-        num1 = parseInt(completo.substring(0, mul));
-        num2 = parseInt(completo.substring(mul + 1, completo.length));
-        res = num1 * num2;
-      } else if (div > 0) {
-        num1 = parseInt(completo.substring(0, div));
-        num2 = parseInt(completo.substring(div + 1, completo.length));
-        res = num1 / num2;
-      }
-      completo = '0';
-      resultado.innerHTML = res;
-    }
-
-    if (op.charAt(0) == '<') {
-      completo = completo.substring(0, completo.length - 1);
-    } else if (op == '=') {
-      completo = completo.substring(0, completo.length - 1);
-    } else {
-      completo += op;
-    }
-    operation.value = completo;
-    if (operation.value == '') {
-      operation.ariaPlaceholder = '0';
-    }
-    console.log(completo);
-  });
-}*/
-const numberBtns = document.querySelectorAll('#Number')
-const operators = document.querySelectorAll('#diferent')
-// const inputNum = () => {
-//   // let number = 0
-//   numberBtns.forEach((button) => {
-//     number = button.innerHTML
-//     console.log(number)
-//   })
-//   document.getElementById("operation").value += number
-// }
 // Function that returns the answer based on the array of values provided and the operator
 const calculate =(arr, operator)=>{
   switch (operator) {
@@ -175,7 +74,6 @@ const calculate =(arr, operator)=>{
   }
 }
 
-
 const clearCalc = () => {
   operation.value = '';
   operation.ariaPlaceholder = '0'
@@ -189,6 +87,7 @@ const initialiseButtonHandler = () => {
     })
   })
 
+  //adding event handlers for the operator buttons
   operators.forEach((button) => {
     button.addEventListener("click", () => {
       if (!button.innerText.includes("Clear") ){
@@ -214,11 +113,33 @@ const initialiseButtonHandler = () => {
 
 }
 
+//Getting Input from the keyboard
+document.onkeydown = function(e){
+  let numberKey = ['0','1','2','3','4','5','6','7','8','9','.']
+  let operatorKeys = ['-','+','*','/','Enter']
+  if ((numberKey.indexOf(e.key)>-1)){
+    operation.value += e.key
+  
+  }else if(operatorKeys.indexOf(e.key)>-1){
+    operation.value += " " + e.key + " ";
+    let numbersArr =  operation.value.split(" ")
+    // Splice the array so that we now have an array for the operator and another array for the numbers
+    let operatorsArr = numbersArr.splice(1, 1)  
+  if(numbersArr.length > 2){
+    // use calculate function to do the calculation based on the operator in the operator array
+    operation.value = calculate(numbersArr, operatorsArr[0] )
+    if (!e.key.includes('Enter') ){
+      operation.value += " " + e.key + " ";
+    }
+  } 
+}else if (e.key == 'Backspace'){
+  clearCalc()
+}
+}
 //<------------ all of this code should be in their own functions so we need to rework this logic ------------>//
 
 const main = () => {
   initialiseClickHandler();
-  initialiseKeyboardHandler();
   initialiseButtonHandler();
 };
 
